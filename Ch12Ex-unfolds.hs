2d64ef15edf75f6023cdb6b2198851650f7e98a7
Daniel Vianna <dvianna@bunnings.com.au>
2016-02-09 15:17:33 +0800

    stuck at myUnfoldr

---
+++ Ch12Ex-unfolds.hs
@@ -0,0 +1,54 @@
+
+import Data.List
+import Data.Maybe
+
+-- Unfolds
+
+mehSum :: Num a => [a] -> a
+mehSum xs = go 0 xs
+    where go :: Num a => a -> [a] -> a
+          go n [] = n
+          go n (x:xs) = go (n + x) xs
+
+niceSum :: Num a => [a] -> a
+niceSum = foldl' (+) 0
+
+mehProduct :: Num a => [a] -> a
+mehProduct xs = go 1 xs
+    where go :: Num a => a -> [a] -> a
+          go n [] = n
+          go n (x:xs) = go (n * x) xs
+
+niceProduct :: Num a => [a] -> a
+niceProduct = foldl' (*) 1
+
+mehConcat :: [[a]] -> [a]
+mehConcat xs = go [] xs
+    where go :: [a] -> [[a]] -> [a]
+          go xs' [] = xs'
+          go xs' (x:xs) = go (xs' ++ x) xs
+
+niceConcat :: [[a]] -> [a]
+niceConcat = foldr (++) []
+
+-----------------------------------------
+
+-- Write your own iterate and unfoldr
+
+-- 1. Write myIterate using direct recursion
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f x = x:(myIterate f (f x))
+
+-- OMG I MADE IT WORK -> No need for the [],
+-- it will never be evaluated.
+
+-- 2. Write myUnfoldr using direct recursion
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr = undefined
+
+-- 3. Stuff that. Rewrite myIterate using unfoldr
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f = unfoldr (\x -> Just (x, f x))
