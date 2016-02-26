7f5d2c61ed95977755c904465dff7d7b6ebc850a
Daniel Vianna <dvianna@bunnings.com.au>
2016-02-26 13:51:21 +0800

    Identity

--- Ch15Ex-Monoid.hs
+++ Ch15Ex-Monoid.hs
@@ -4,6 +4,18 @@ import Data.Semigroup (Semigroup, (<>))
 import Test.QuickCheck (quickCheck, Arbitrary, arbitrary, Gen)
 
 -- Monoid exercises
+
+-- Semigroup and Monoid tests
+
+semigroupAssoc :: (Eq s, Semigroup s) => s -> s -> s -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
+monoidLeftIdentity a = (a <> mempty) == a
+
+monoidRightIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
+monoidRightIdentity a = (mempty <> a) == a
+
 -- 1. Trivial
 
 data Trivial = Trivial deriving (Eq, Show)
@@ -20,19 +32,44 @@ instance Arbitrary Trivial where
 
 type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
 
--- Semigroup and Monoid tests
+-- 2. Identity
 
-semigroupAssoc :: (Eq s, Semigroup s) => s -> s -> s -> Bool
-semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+newtype Identity a = Identity a deriving Show
 
-monoidLeftIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
-monoidLeftIdentity a = (a <> mempty) == a
+-- Id Instances
 
-monoidRightIdentity :: (Eq m, Monoid m, Semigroup m) => m -> Bool
-monoidRightIdentity a = (mempty <> a) == a
+instance Semigroup a => Semigroup (Identity a) where
+  Identity a <> Identity a' = Identity (a <> a')
+
+instance (Monoid a, Semigroup a) => Monoid (Identity a) where
+  mempty = Identity mempty
+  mappend = (<>)
+
+instance Eq a => Eq (Identity a) where
+  Identity a == Identity a' = a == a'
+
+instance (Arbitrary a, Monoid a, Semigroup a) =>
+    Arbitrary (Identity a) where
+      arbitrary = genId
+
+-- ID generator
+
+genId :: Arbitrary a => Gen (Identity a)
+genId = do
+  x <- arbitrary
+  return $ Identity x
+
+type S = String
+type Id = Identity
+type IdAssoc = Id S -> Id S -> Id S -> Bool 
 
 main :: IO ()
 main = do
+  putStrLn "\n Trivial"
   quickCheck (semigroupAssoc :: TrivialAssoc)
   quickCheck (monoidLeftIdentity :: Trivial -> Bool)
   quickCheck (monoidRightIdentity :: Trivial -> Bool)
+  putStrLn "\n Identity"
+  quickCheck (semigroupAssoc :: IdAssoc)
+  quickCheck (monoidLeftIdentity :: Id S -> Bool)
+  quickCheck (monoidRightIdentity :: Id S -> Bool)
