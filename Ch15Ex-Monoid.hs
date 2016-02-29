20f5e2991e7412446907c1d2e876d6d6dc598ffb
Daniel Vianna <dvianna@bunnings.com.au>
2016-02-29 13:19:48 +0800

    'Or' doesn't seem to be a monoid

--- Ch15Ex-Monoid.hs
+++ Ch15Ex-Monoid.hs
@@ -1,7 +1,11 @@
 
 import Data.Monoid (Monoid, mempty, mappend)
 import Data.Semigroup (Semigroup, (<>))
-import Test.QuickCheck (quickCheck, Arbitrary, arbitrary, Gen)
+import Test.QuickCheck (Arbitrary,
+                        arbitrary,
+                        elements,
+                        Gen,
+                        quickCheck)
 
 -- Monoid exercises
 
@@ -104,6 +108,115 @@ testTwoEq :: (Eq a, Eq b) => Two a b -> Two a b -> Bool
 testTwoEq x x' = (\ (Two a b) (Two a' b') -> (a == a') && (b == b')) x x' ==
                  (x == x')
 
+-- 4. BoolConj
+
+newtype BoolConj = BoolConj Bool
+
+instance Semigroup BoolConj where
+  BoolConj True <> BoolConj True = BoolConj True
+  BoolConj _ <> BoolConj _ = BoolConj False
+
+instance Monoid BoolConj where
+  mempty = BoolConj True
+  mappend = (<>)
+
+instance Eq BoolConj where
+  BoolConj a == BoolConj a' = a == a'
+
+instance Show BoolConj where
+  show (BoolConj a) = "BoolConj " ++ show a
+
+instance Arbitrary BoolConj where
+  arbitrary = genBConj
+
+genBConj :: Gen BoolConj
+genBConj = elements [BoolConj False, BoolConj True]
+
+-- test BoolConj
+
+type BCAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+testBCEq :: BoolConj -> BoolConj -> Bool
+testBCEq x x' = (\ (BoolConj a) (BoolConj b) -> a == b) x x' == (x == x')
+
+testBCShow :: BoolConj -> Bool
+testBCShow x = (\ (BoolConj a) -> "BoolConj " ++ show a) x == show x
+
+testBC :: BoolConj -> BoolConj -> Bool
+testBC x x' = (\ (BoolConj a) (BoolConj b) -> a && b) x x' ==
+              (\ (BoolConj a) -> a) (x <> x')
+
+-- 5. BoolDisj
+
+newtype BoolDisj = BoolDisj Bool
+
+instance Semigroup BoolDisj where
+  BoolDisj False <> BoolDisj False = BoolDisj False
+  BoolDisj _ <> BoolDisj _ = BoolDisj True
+
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+  mappend = (<>)
+
+instance Eq BoolDisj where
+  BoolDisj a == BoolDisj a' = a == a'
+
+instance Show BoolDisj where
+  show (BoolDisj a) = "BoolDisj " ++ show a
+
+instance Arbitrary BoolDisj where
+  arbitrary = genBDisj
+
+genBDisj :: Gen BoolDisj
+genBDisj = elements [BoolDisj False, BoolDisj True]
+
+-- test BoolDisj
+
+type BDAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+testBDEq :: BoolDisj -> BoolDisj -> Bool
+testBDEq x x' = (\ (BoolDisj a) (BoolDisj b) -> a == b) x x' == (x == x')
+
+testBDShow :: BoolDisj -> Bool
+testBDShow x = (\ (BoolDisj a) -> "BoolDisj " ++ show a) x == show x
+
+testBD :: BoolDisj -> BoolDisj -> Bool
+testBD x x' = (\ (BoolDisj a) (BoolDisj b) -> a || b) x x' ==
+              (\ (BoolDisj a) -> a) (x <> x')
+
+-- 6. Or
+
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  Fst _ <> Snd x = Snd x
+  Fst _ <> Fst x = Fst x
+  Snd x <> Fst _ = Snd x
+  Snd x <> Snd _ = Snd x
+
+instance Monoid a => Monoid (Or a b) where
+  mempty = Fst mempty
+  mappend = (<>)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = genOr
+
+genOr :: (Arbitrary a, Arbitrary b) => Gen (Or a b)
+genOr = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Fst a, Snd b]
+
+testOr :: (Eq a, Eq b) => Or a b -> Or a b -> Bool
+testOr x x' = case (x, x') of
+  (Fst _, Snd a) -> x <> x' == Snd a
+  (Fst _, Fst a) -> x <> x' == Fst a
+  (Snd a, Fst _) -> x <> x' == Snd a
+  (Snd a, Snd _) -> x <> x' == Snd a
+
+type OrAssoc = Or S S -> Or S S -> Or S S -> Bool
+
+
 -- main
 
 main :: IO ()
@@ -122,3 +235,23 @@ main = do
   quickCheck (monoidLeftIdentity :: Two S S -> Bool)
   quickCheck (monoidRightIdentity :: Two S S -> Bool)
   quickCheck (testTwoEq :: Two S S -> Two S S -> Bool)
+  putStrLn "\n BoolConj"
+  quickCheck (semigroupAssoc :: BCAssoc)
+  quickCheck (monoidLeftIdentity :: BoolConj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolConj -> Bool)
+  quickCheck (testBCEq :: BoolConj -> BoolConj -> Bool)
+  quickCheck (testBCShow :: BoolConj -> Bool)
+  quickCheck (testBC :: BoolConj -> BoolConj -> Bool)
+  putStrLn "\n BoolDisj"
+  quickCheck (semigroupAssoc :: BDAssoc)
+  quickCheck (monoidLeftIdentity :: BoolDisj -> Bool)
+  quickCheck (monoidRightIdentity :: BoolDisj -> Bool)
+  quickCheck (testBDEq :: BoolDisj -> BoolDisj -> Bool)
+  quickCheck (testBDShow :: BoolDisj -> Bool)
+  quickCheck (testBD :: BoolDisj -> BoolDisj -> Bool)
+  putStrLn "\n Or"
+  quickCheck (semigroupAssoc :: OrAssoc)
+  putStrLn "No left identity, as Fst a <> Fst b = Fst b"
+  quickCheck (monoidLeftIdentity :: Or S S -> Bool)
+  quickCheck (monoidRightIdentity :: Or S S -> Bool)
+  quickCheck (testOr :: Or S S -> Or S S -> Bool)
