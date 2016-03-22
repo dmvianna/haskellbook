51beb5b5d20bd91f920a4f0cc25c3163429e2223
Daniel Vianna <dvianna@bunnings.com.au>
2016-03-22 14:16:16 +0800

    Applicative List

--- Ch17-Applicative-zipList.hs
+++ Ch17-Applicative-zipList.hs
@@ -27,17 +27,29 @@ instance Functor List where
   fmap f Nil = Nil
   fmap f (Cons x xs) = Cons (f x) (fmap f xs)
 
--- Gives wrong result
-instance Applicative List where
-  pure x = Cons x Nil
-  Nil <*> _ = Nil
-  _ <*> Nil = Nil
-  Cons x xs <*> Cons x' xs' = Cons (x x') (xs <*> xs')
-
--- instance Monoid (List a) where
---   mempty = Nil
---   mappend =
+instance Monoid (List a) where
+  mempty = Nil
+  mappend = append
 
 append :: List a -> List a -> List a
 append Nil ys = ys
 append (Cons x xs) ys = Cons x (append xs ys)
+
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil = b
+fold f b (Cons h t) = f h (fold f b t)
+
+concat' :: List (List a) -> List a
+concat' = fold append Nil
+
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' $ fmap f as
+
+instance Applicative List where
+  pure x = Cons x Nil
+  _ <*> Nil = Nil
+  Nil <*> _ = Nil
+  Cons f fs <*> xs = append (flatMap (fmap f) (Cons xs Nil)) (fs <*> xs)
+
+-- flatMap (fmap (+1)) $ Cons (Cons 4 Nil) (Cons (Cons 5 Nil) Nil)
+-- Cons 5 (Cons 6 Nil)
