f6cfaa59b0120e08d42d3902b2d4130132048986
Daniel Vianna <dvianna@bunnings.com.au>
2016-03-23 14:25:07 +0800

    ZipList Applicative bones

--- Ch17-Applicative-zipList.hs
+++ Ch17-Applicative-zipList.hs
@@ -53,3 +53,29 @@ instance Applicative List where
 
 -- flatMap (fmap (+1)) $ Cons (Cons 4 Nil) (Cons (Cons 5 Nil) Nil)
 -- Cons 5 (Cons 6 Nil)
+
+-- let functions = Cons (+1) (Cons (*2) Nil)
+-- let values = Cons 1 (Cons 2 Nil)
+-- functions <*> values
+-- Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil)))
+
+-- ZipList Applicative
+
+take' :: Int -> List a -> List a
+take' = undefined
+
+newtype ZipList' a = ZipList' (List a) deriving (Eq, Show)
+
+instance Eq a => EqProp (ZipList' a) where
+    xs =-= ys = xs' `eq` ys'
+        where xs' = let (ZipList' l) = xs
+                    in take' 3000 l
+              ys' = let (ZipList' l) = ys
+                    in take' 3000 l
+
+instance Functor ZipList' where
+  fmap f (ZipList' xs) = ZipList' $ fmap f xs
+
+instance Applicative ZipList' where
+  pure = undefined
+  (<*>) = undefined
