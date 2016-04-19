f14c3aae48124198ad60f7754a0c114d1562276d
Daniel Vianna <dvianna@bunnings.com.au>
2016-04-19 14:56:28 +0800

    Chapter 20 Exercises - Foldable

---
+++ Ch20-Foldable.hs
@@ -0,0 +1,25 @@
+
+import Data.Foldable
+import Data.Monoid
+
+data Identity a = Identity a
+
+instance Foldable Identity where
+  foldr f z (Identity x) = f x z
+  foldl f z (Identity x) = f z x
+  foldMap f (Identity x) = f x
+
+data Optional a = Nada | Yep a
+
+instance Foldable Optional where
+  foldr _ z Nada = z
+  foldr f z (Yep x) = f x z
+
+  foldl _ z Nada = z
+  foldl f z (Yep x) = f z x
+
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
+
+mySum :: (Foldable t, Num a) => t a -> a
+mySum x = getSum $ foldMap Sum x
