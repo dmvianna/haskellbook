fac2871ff0d66fdc25a0100aea887d3f2a450bbe
Daniel Vianna <dvianna@bunnings.com.au>
2016-04-28 12:36:02 +0800

    Reader Applicative instance

--- Ch22-Reader.hs
+++ Ch22-Reader.hs
@@ -56,3 +56,74 @@ tupled'' = do
 
 tupled''' :: [Char] -> ([Char], [Char])
 tupled''' xs = fmap rev $ (cap >>= (,)) (xs)
+
+-- Another exercise
+
+newtype Reader r a = Reader { runReader :: r -> a }
+
+ask :: Reader a a
+ask = Reader id
+
+-- Demo
+
+newtype HumanName = HumanName String deriving (Eq, Show)
+newtype DogName = DogName String deriving (Eq, Show)
+newtype Address = Address String deriving (Eq, Show)
+
+data Person = Person {
+      humanName :: HumanName
+    , dogName :: DogName
+    , address :: Address
+    } deriving (Eq, Show)
+
+data Dog = Dog {
+      dogsName :: DogName
+    , dogsAddress :: Address
+    } deriving (Eq, Show)
+
+pers :: Person
+pers = Person
+       (HumanName "Big Bird")
+       (DogName "Barkley")
+       (Address "Sesame Street")
+
+chris :: Person
+chris = Person
+        (HumanName "Chris Allen")
+        (DogName "Papu")
+        (Address "Austin")
+
+getDog :: Person -> Dog
+getDog p = Dog (dogName p) (address p)
+
+getDogR :: Person -> Dog
+getDogR = Dog <$> dogName <*> address
+
+getDogR' :: Person -> Dog
+getDogR' = liftA2 Dog dogName address
+
+-- Exercise
+
+myLiftA2 :: Applicative f =>
+            (a -> b -> c)
+         -> f a -> f b -> f c
+myLiftA2 f a b = f <$> a <*> b
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
+
+instance Functor (Reader a) where
+  fmap f (Reader { runReader = x }) =
+    Reader { runReader = f . x }
+
+instance Applicative (Reader r) where
+  pure a = Reader (\r -> a)
+  Reader { runReader = f } <*>
+    Reader { runReader = g } =
+      Reader { runReader = (\r -> f r (g r)) }
+
+getDogR'' :: Reader Person Dog
+getDogR'' = Dog <$> Reader dogName <*> Reader address
+
+-- Monad of functions
+
