91c4f0bf8d1857f4e4a172be961d98c027351cc0
Daniel Vianna <dvianna@bunnings.com.au>
2016-05-25 14:23:38 +0800

    Applicative and Monad

--- Ch23-State.hs
+++ Ch23-State.hs
@@ -3,8 +3,12 @@
 newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 
 instance Functor (Moi s) where
-  fmap :: (a -> b) -> Moi s a -> Moi s b
-  fmap f (Moi g) = Moi $ (\(a, s) -> (f a, s)) . g
+  fmap :: (a -> b)
+       -> Moi s a
+       -> Moi s b
+  fmap f (Moi g) = Moi $ \s -> let (a, b) = g s
+                               in (f a, b)
+  
 
 instance Applicative (Moi s) where
   pure :: a -> Moi s a
@@ -12,4 +16,15 @@ instance Applicative (Moi s) where
   (<*>) :: Moi s (a -> b)
         -> Moi s a
         -> Moi s b
-  (Moi f) <*> (Moi g) = undefined -- Moi $ f . g
+  (Moi f) <*> (Moi g) = Moi $ \s -> let (a, b) = g s
+                                        (fab, _) = f s
+                                    in (fab a, b)
+
+instance Monad (Moi s) where
+  return = pure
+  (>>=) :: Moi s a
+        -> (a -> Moi s b)
+        -> Moi s b
+  (Moi f) >>= g = Moi $ \s -> let a = fst $ f s
+                                  ms = runMoi $ g a
+                              in ms s
