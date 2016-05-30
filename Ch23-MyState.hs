b8001e1f6662697ee1bac136b3b2a25d45e9725a
Daniel Vianna <dvianna@bunnings.com.au>
2016-05-30 10:05:50 +0800

    modify nailed

--- Ch23-MyState.hs
+++ Ch23-MyState.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 
 newtype State s a = State { runState :: s -> (a, s) }
 
@@ -15,7 +16,40 @@ eval :: State s a -> s -> a
 eval (State sa) x = let (a, _) = sa x
                     in a
 
--- not quite perfect yet
+-- We need instances here
+
+instance Functor (State s) where
+  fmap :: (a -> b)
+       -> State s a
+       -> State s b
+  fmap f (State g) = State $ \s -> let (a, b) = g s
+                                   in (f a, b)
+
+instance Applicative (State s) where
+  pure :: a -> State s a
+  pure a = State $ \s -> (a, s)
+  (<*>) :: State s (a -> b)
+        -> State s a
+        -> State s b
+  (State f) <*> (State g) = State $ \s -> let (a, b) = g s
+                                              (fab, _) = f s
+                                          in (fab a, b)
+
+instance Monad (State s) where
+  return = pure
+  (>>=) :: State s a
+        -> (a -> State s b)
+        -> State s b
+  (State f) >>= g = State $ \s -> let a = fst $ f s
+                                      ms = runState $ g a
+                                  in ms s
+  (>>) :: State s a
+       -> State s b
+       -> State s b
+  State f >> State g = State $ \s -> let (_, s') = f s
+                                     in g s'
+
+--
 
 modify :: (s -> s) -> State s ()
 modify f = State $ \x -> ((), f x)
