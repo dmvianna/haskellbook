5b316e630c19496ec975933b9aa198b9c6c44ef0
Daniel Vianna <dvianna@bunnings.com.au>
2016-05-31 13:37:10 +0800

    parsing "1", "12", "123" without issues

--- Ch24-LearnParsers.hs
+++ Ch24-LearnParsers.hs
@@ -1,30 +1,18 @@
+{-# LANGUAGE RankNTypes #-}
 
 module LearnParsers where
 
 import Text.Trifecta
+import Text.Parser.Combinators
 
 stop :: Parser a
 stop = unexpected "stop"
 
 one = char '1'
+-- two = char '2'
+-- three = char '3'
 
 one' = one >> stop
-
--- char :: Char -> Parser Char
--- char c =
---   Parser $ \s ->
---       case s of
---         (x:xs) -> if c == x
---                   then [(c, xs)]
---                   else []
---         _ -> []
-
--- type Token = Char
-
--- newtype Parser a = P ([Token] -> [(a, [Token])])
-
--- type Parser' a = String -> [(a, String)]
-
 
 -- read two characters, '1' and '2'
 oneTwo = char '1' >> char '2'
@@ -35,6 +23,32 @@ oneTwo' = oneTwo >> stop
 testParse :: Parser Char -> IO ()
 testParse p =
   print $ parseString p mempty "123"
+
+testEOF :: Parser () -> IO ()
+testEOF p =
+  print $ parseString p mempty "123"
+
+-- string parsers
+
+type S = forall m. CharParsing m => m String
+
+oneS :: S
+oneS = string "1"
+
+oneTwoS :: S
+oneTwoS = string "12"
+
+oneTwoThreeS :: S
+oneTwoThreeS = string "123"
+
+testParse' :: Parser String -> IO ()
+testParse' p =
+  print $ parseString p mempty "123"
+
+-- One Parser rules them all
+-- how do we prevent >> to drop it on the floor?
+
+--
 
 pNL s = putStrLn ('\n' : s)
 
@@ -49,4 +63,12 @@ main = do
   testParse oneTwo
   pNL "oneTwo':"
   testParse oneTwo'
-
+  pNL "one >> EOF:"
+  testEOF (one >> eof)
+  pNL "oneTwo >> EOF"
+  testEOF (oneTwo >> eof)
+  pNL "string \"1\", \"12\", \"123\""
+  testParse' (choice [ oneTwoThreeS
+                     , oneTwoS
+                     , oneS
+                     , stop ])
