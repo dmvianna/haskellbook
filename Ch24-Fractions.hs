2ced0edf2fae3f2d33e93f9b4e73af48bff24f42
Daniel Vianna <dvianna@bunnings.com.au>
2016-05-31 14:26:57 +0800

    virtuousFraction

---
+++ Ch24-Fractions.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Text.Fractions where
+
+import Control.Applicative
+import Data.Ratio ((%))
+import Text.Trifecta
+
+badFraction = "1/0"
+alsoBad = "10"
+shouldWork = "1/2"
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  return (numerator % denominator)
+
+main :: IO ()
+main = do
+  print $ parseString parseFraction mempty shouldWork
+  print $ parseString parseFraction mempty shouldAlsoWork
+  print $ parseString parseFraction mempty alsoBad
+  print $ parseString parseFraction mempty badFraction
+
+virtuousFraction :: Parser Rational
+virtuousFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  case denominator of
+    0 -> fail "Denominator cannot be zero"
+    _ -> return (numerator % denominator)
+
+testVirtuous :: IO ()
+testVirtuous = do
+  print $ parseString virtuousFraction mempty badFraction
+  print $ parseString virtuousFraction mempty alsoBad
+  print $ parseString virtuousFraction mempty shouldWork
+  print $ parseString virtuousFraction mempty shouldAlsoWork
+
