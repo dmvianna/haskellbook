25ad2f318f6764ff66bad28b11c2284245c88c76
Daniel Vianna <dvianna@bunnings.com.au>
2016-06-22 12:20:29 +0800

    Any integer

--- Ch24-PositiveInteger.hs
+++ Ch24-PositiveInteger.hs
@@ -5,8 +5,15 @@ import Control.Applicative
 import Text.Trifecta
 
 parseDigit :: Parser Char
-parseDigit = oneOf "0123456789"
-  
+parseDigit = oneOf "0123456789" <?> "a digit between 0 and 9"
 
 base10Integer :: Parser Integer
 base10Integer = read <$> some parseDigit
+
+base10Integer' :: Parser Integer
+base10Integer' = do
+  sign <- optional (char '-')
+  xs <- some parseDigit
+  case sign of
+    Nothing -> return (read xs)
+    Just x -> return (read $ x:xs)
