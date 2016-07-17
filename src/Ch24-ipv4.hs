63e73de485a3ae10c0997a0670e519cfaa0b060d
Daniel Vianna <dvianna@bunnings.com.au>
2016-07-18 07:45:16 +0800

    drop State, it isn't needed.

--- src/Ch24-ipv4.hs
+++ src/Ch24-ipv4.hs
@@ -1,10 +1,6 @@
 
 module Ipv4 where
 
--- I embarrass myself trying to use State but instead
--- just doing complicated recursion
-
-import Control.Monad.Trans.State
 import Data.Word
 import Test.Hspec
 import Text.Trifecta
@@ -17,20 +13,18 @@ type Rem = Integer
 type Bit = Integer
 type Pos = Integer
 
-spewBit :: State (Rem, [Bit], Pos) (Rem, [Bit], Pos)
-spewBit = do
-  (i, xs, p) <- get
+spewBit :: (Rem, [Bit], Pos) -> (Rem, [Bit], Pos)
+spewBit (i, xs, p) =
   let p' = p - 1
       b = 2 ^ p'
       (q, r) = i `quotRem` b
-  put (r, q:xs, p') -- That is obviously silly.
-  return (r, xs, p') -- So is this.
+  in (r, q:xs, p')
 
 spewPart :: (Rem, [Bit], Pos) -> [Bit]
 spewPart (r,bs,p) =
   if p == 0
   then reverse bs
-  else spewPart $ execState spewBit (r,bs,p) -- no idea from where execState came from.
+  else spewPart $ spewBit (r,bs,p)
 
 initState :: Pos -> Integer -> (Rem, [Bit], Pos)
 initState p n = (n, [], p)
@@ -47,27 +41,24 @@ parseIPv4 = do
   let xs = concat $ spewPart <$> initState 8 <$> [n,n',sn,h]
   return xs
 
-spewInteger :: State (Integer, [Bit]) (Integer, [Bit])
-spewInteger = do
-  (s, xs) <- get
+spewInteger :: (Integer, [Bit]) -> (Integer, [Bit])
+spewInteger (s, xs) =
   let l = length xs - 1
       t = 2 ^ l
-      r =  (s + head xs * t, tail xs)
-  put r
-  return r
+  in (s + head xs * t, tail xs)
 
 bitToInteger :: (Integer, [Bit]) -> Word32
 bitToInteger (i, xs) =
   if null xs
   then fromInteger i
-  else bitToInteger $ execState spewInteger (i, xs)
+  else bitToInteger $ spewInteger (i, xs)
 
 main :: IO ()
 main = hspec $ do
 
   describe "Test some IP values" $ do
     let ip1 = "172.16.254.1"
-    let ip2 = "204.120.0.15"
+        ip2 = "204.120.0.15"
     it ("can parse " ++ ip1) $ do
       -- Nonexhaustive, I know
       let (Success x) = parseString parseIPv4 mempty ip1
