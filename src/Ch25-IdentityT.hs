b5d820eaa46a43d9275bc6870f8686121026c9d9
Daniel Vianna <dvianna@bunnings.com.au>
2016-07-21 10:19:38 +0800

    IdentityT

---
+++ src/Ch25-IdentityT.hs
@@ -0,0 +1,35 @@
+
+module IdentityT where
+
+newtype Identity a =
+  Identity { runIdentity :: a }
+  deriving (Eq, Show)
+
+newtype IdentityT f a =
+  IdentityT { runIdentityT :: f a }
+  deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance (Functor m) => Functor (IdentityT m) where
+  fmap f (IdentityT fa) = IdentityT (fmap f fa)
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity a) = Identity (f a)
+
+instance (Applicative m) => Applicative (IdentityT m) where
+  pure x = IdentityT (pure x)
+  (IdentityT fab) <*> (IdentityT fa) =
+    IdentityT (fab <*> fa)
+
+instance Monad Identity where
+  return = pure
+  (Identity a) >>= f = f a
+
+instance (Monad m) => Monad (IdentityT m) where
+  return = pure
+  (IdentityT ma) >>= f =
+    IdentityT $ ma >>= runIdentityT . f
+
