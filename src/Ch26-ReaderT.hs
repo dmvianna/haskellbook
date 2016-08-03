76ebb1e55b57f547a756c2eb7cf010c1259a63f1
Daniel Vianna <dmlvianna@gmail.com>
2016-08-03 00:58:47 +0000

    ReaderT Functor instance - not working

--- src/Ch26-ReaderT.hs
+++ src/Ch26-ReaderT.hs
@@ -2,6 +2,8 @@
 
 module ReaderT where
 
+import Data.Bifunctor
+
 newtype ReaderT r m a =
   ReaderT { runReaderT :: r -> m a }
 
@@ -22,3 +24,16 @@ instance Monad m => Monad (ReaderT r m) where
     ReaderT $ \r -> do
     a <- rma r
     runReaderT (f a) r
+
+
+newtype StateT s m a =
+  StateT { runStateT :: s -> m (a, s) }
+
+instance (Functor m) => Functor (StateT s m) where
+  fmap :: (a -> b)
+       -> StateT s m a
+       -> StateT s m b
+  fmap f (StateT sma) = StateT $
+    \s -> let r = sma s
+          in bimap f (snd . fst) (runStateT r)
+
