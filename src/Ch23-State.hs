3a74d8e00bbb6dcc4c585f5fb863b7ebd948e261
Daniel Vianna <dmlvianna@gmail.com>
2016-08-08 13:24:30 +0000

    State still can't use Checkers

--- src/Ch23-State.hs
+++ src/Ch23-State.hs
@@ -1,14 +1,12 @@
 {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE DeriveGeneric #-}
 
 module Moi where
 
-import GHC.Generics
 import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
-newtype Moi s a = Moi { runMoi :: s -> (a, s) } deriving (Show, Generic)
+newtype Moi s a = Moi { runMoi :: s -> (a, s) }
 
 instance Functor (Moi s) where
   fmap :: (a -> b)
@@ -42,19 +40,9 @@ genMoi :: (Arbitrary s, Arbitrary a, CoArbitrary s, CoArbitrary a)
           => Gen (s -> (a, s))
 genMoi = arbitrary
 
-instance (Arbitrary s, Arbitrary a, CoArbitrary s, CoArbitrary a)
-         => Arbitrary (Moi s a) where
-  arbitrary = do
-    f <- genMoi
-    return $ Moi f
-
-instance (Eq a, Eq s) => EqProp (Moi s a) where
-  (=-=) = eq
-
-
 main :: IO ()
 main = do
-  let trigger = undefined :: Moi (Int -> (String, Int)) (Int, Int, [Int])
-  quickBatch $ functor trigger
-  quickBatch $ applicative trigger
-  quickBatch $ monad trigger
+  let trigger = undefined :: (Bool, Int, [Int])
+  -- quickBatch $ functor trigger
+  -- quickBatch $ applicative trigger
+  quickBatch $ monad (pure trigger :: Moi String (Bool, Int, [Int]))
