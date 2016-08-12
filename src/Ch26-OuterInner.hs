de4dac24eaba77282a663dbe02fcbf9c154db057
Daniel Vianna <dmlvianna@gmail.com>
2016-08-12 01:40:22 +0000

    OuterInner + Scotty

---
+++ src/Ch26-OuterInner.hs
@@ -0,0 +1,18 @@
+
+module OuterInner where
+
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+
+embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
+embedded = return 1
+
+maybeUnwrap :: ExceptT String (ReaderT () IO) (Maybe Int)
+maybeUnwrap = runMaybeT embedded
+
+eitherUnwrap :: ReaderT () IO (Either String (Maybe Int))
+eitherUnwrap = runExceptT maybeUnwrap
+
+readerUnwrap :: () -> IO (Either String (Maybe Int))
+readerUnwrap = runReaderT eitherUnwrap
---
+++ src/Ch26-Scotty.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scotty where
+
+import Web.Scotty
+import Control.Monad.Trans.Class
+import Data.Monoid (mconcat)
+
+main = scotty 3000 $ do
+  get ":/word" $ do
+    beam <- param "word"
+    lift $ putStrLn "hello"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!<h1>"]
