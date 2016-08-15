e9655b75aed4d205e1d36efe3e22911cf6a5b544
Daniel Vianna <dvianna@bunnings.com.au>
2016-08-15 14:56:35 +0800

    Scotty exposed

--- src/Ch26-Scotty.hs
+++ src/Ch26-Scotty.hs
@@ -3,12 +3,24 @@
 module Scotty where
 
 import Web.Scotty
-import Control.Monad.Trans.Class
-import Data.Monoid (mconcat)
+import Web.Scotty.Internal.Types (ActionT(..))
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Lazy hiding (get)
 
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT m = ReaderT (const m)
+
+main :: IO ()
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    lift $ putStrLn "hello"
+    (ActionT
+      . (ExceptT . fmap Right)
+      . ReaderT . const
+      . \m -> StateT (\s -> do
+                         a <- m
+                         return (a, s))
+      ) (putStrLn "hello")
     html $ mconcat ["<h1>Scotty, ", beam, " me up!<h1>"]
 
