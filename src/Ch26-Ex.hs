5693d7b2fdb84459949573e533513b24db24a841
Daniel Vianna <dvianna@bunnings.com.au>
2016-08-19 11:47:58 +0800

    sPrintIntAccum

--- src/Ch26-Ex.hs
+++ src/Ch26-Ex.hs
@@ -5,6 +5,7 @@ module Exercises where
 
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
 import Data.Functor.Identity
 
 rDec :: Num a => Reader a a
@@ -15,5 +16,10 @@ rShow = ReaderT $ Identity . show
 
 rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
 rPrintAndInc = ReaderT $ \r -> do
-  liftIO $ putStrLn $ "hi: " ++ show r
+  liftIO . putStrLn $ "hi: " ++ show r
   return $ r + 1
+
+sPrintIncAccum :: (Num a, Show a) => StateT a IO String
+sPrintIncAccum = StateT $ \s -> do
+  liftIO . putStrLn $ "Hi: " ++ show s
+  return $ (show s, s + 1)
