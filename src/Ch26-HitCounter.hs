39ce3a22b0b6270f8c0010e2a0f2acb0e9e9f31f
Daniel Vianna <dvianna@bunnings.com.au>
2016-08-19 14:33:57 +0800

    hit counter -- initial state

---
+++ src/Ch26-HitCounter.hs
@@ -0,0 +1,58 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+import Data.IORef
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as TL
+import System.Environment (getArgs)
+import Web.Scotty.Trans
+
+data Config =
+  Config {
+    -- that's one, one click!
+    -- two...two clicks!
+    -- Three BEAUTIFUL clicks! ah ah ahhhh
+    counts :: IORef (M.Map Text Integer)
+    , prefix :: Text
+    }
+
+-- Stuff inside ScottyT is, except for things that escape
+-- via IO, effectively read-only so we can't use StateT.
+-- It would overcomplicate things to attempt to do so and
+-- you should be using a proper database for production
+-- applications.
+
+type Scotty = ScottyT Text (ReaderT Config IO)
+type Handler = ActionT Text (ReaderT Config IO)
+
+bumpBoomp :: Text
+          -> M.Map Text Integer
+          -> (M.Map Text Integer, Integer)
+bumpBoomp k m = undefined
+
+app :: Scotty ()
+app =
+  get "/:key" $ do
+    unprefixed <- param "key"
+    let key' = mappend undefined unprefixed
+    newInteger <- undefined
+    html $ mconcat [ "<h1>Success! Count was: "
+                   , TL.pack $ show newInteger
+                   , "</h1>"
+                   ]
+
+main :: IO ()
+main = do
+  [prefixArg] <- getArgs
+  counter <- newIORef M.empty
+  let config = undefined
+      runR = undefined
+  scottyT 3000 runR app
+
+    
