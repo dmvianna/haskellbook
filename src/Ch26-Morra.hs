e2a1efbb69120cb51efb8ebadc387e8db9dc5c70
Daniel Vianna <dvianna@bunnings.com.au>
2016-09-20 13:32:14 +0800

    Morra initial commit

---
+++ src/Ch26-Morra.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Morra where
+
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import Data.IORef
+import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as TL
+import System.IO
+import System.Random
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "P is player"
+  putStrLn "C is computer"
+  putStrLn "Player is odds, computer is evens."
+  putStr "P: "
+  guess <- getLine
+  let cpGuess = 1 :: Int
+      cpShow = show cpGuess
+  putStrLn ("C: " ++ cpShow)
+  case odd $ cpGuess + read guess of
+    True -> putStrLn "P wins"
+    False -> putStrLn "C wins"
+
+  
+  
