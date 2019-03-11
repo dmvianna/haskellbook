b1fdce66283ccf3e740db0bb1ad9730a7669d1fc
Matthias Braun <mb720@users.noreply.github.com>
2019-03-11 22:28:52 +0100

    Update Ch26-MonadTrans.hs (#16)
    I'd like to offer solutions to the MonadTrans and MonadIO exercises. They contain type annotations as well as comments and don't require UndecidableInstances or FlexibleContexts.

--- src/Ch26-MonadTrans.hs
+++ src/Ch26-MonadTrans.hs
@@ -1,70 +1,60 @@
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE InstanceSigs        #-}
 
 module MonadTrans where
 
-import Control.Monad.IO.Class
-import Control.Monad.Trans.Class
-import Control.Monad (liftM)
+import           Control.Monad
 
-newtype IdentityT m a =
-    IdentityT { runIdentityT :: m a }
-      deriving (Eq, Show)
+import           Control.Monad.IO.Class    (MonadIO (liftIO))
+import           Control.Monad.Trans.Class (MonadTrans (lift))
 
 instance MonadTrans IdentityT where
+  lift :: Monad m => m a -> IdentityT m a
   lift = IdentityT
 
-instance (MonadIO m
-         , Monad (IdentityT m))
-         => MonadIO (IdentityT m) where
-  liftIO = IdentityT . liftIO
-
-
-newtype MaybeT m a =
-    MaybeT { runMaybeT :: m (Maybe a) }
-
 instance MonadTrans MaybeT where
-  lift = MaybeT . liftM Just
-
-instance (MonadIO m
-         , Monad (MaybeT m))
-         => MonadIO (MaybeT m) where
-  liftIO = lift . liftIO
-
-
-newtype ReaderT r m a =
-  ReaderT { runReaderT :: r -> m a }
+  lift :: Monad m => m a -> MaybeT m a
+  lift ma = MaybeT $ fmap Just ma
 
 instance MonadTrans (ReaderT r) where
-  lift = ReaderT . const
-
-instance (MonadIO m
-         , Monad (ReaderT r m))
-         => MonadIO (ReaderT r m) where
-  liftIO = lift . liftIO
-
-
-newtype EitherT e m a =
-  EitherT { runEitherT :: m (Either e a) }
+  -- Equivalent to ReaderT const
+  lift ma = ReaderT $ \r -> ma
 
 instance MonadTrans (EitherT e) where
-  lift = EitherT . liftM Right
-
-instance (MonadIO m
-         , Monad (EitherT e m))
-         => MonadIO (EitherT e m) where
-  liftIO = lift . liftIO
-
-
-newtype StateT s m a =
-  StateT { runStateT :: s -> m (a, s) }
+  -- Pointfree version of: lift ma = EitherT $ fmap Right ma
+  lift = EitherT . fmap Right
 
 instance MonadTrans (StateT s) where
-  lift m = StateT $ \s -> do
-    a <- m
-    return (a, s)
+  lift ma = StateT $ \s -> fmap (\a -> (a, s)) ma
 
-instance (MonadIO m
-         , Monad (StateT s m))
-         => MonadIO (StateT s m) where
+instance MonadIO m => MonadIO (IdentityT m) where
+  liftIO :: IO a -> IdentityT m a
+  liftIO ioA = IdentityT ma
+    -- Use m's liftIO to put ioA into m
+    where
+      ma = liftIO ioA
+
+instance MonadIO m => MonadIO (EitherT e m) where
+  liftIO :: IO a -> EitherT e m a
+  -- This is m's liftIO and EitherT's lift
   liftIO = lift . liftIO
+-- Equivalent to:
+--  liftIO ioA = EitherT $ fmap Right ma
+--    where ma = liftIO ioA
+
+instance MonadIO m => MonadIO (MaybeT m) where
+  liftIO = lift . liftIO
+-- Equivalent to:
+--  liftIO ioA = MaybeT mMaybeA
+--    where mMaybeA = fmap Just ma
+--          ma = liftIO ioA
+
+instance MonadIO m => MonadIO (ReaderT r m) where
+  liftIO ioA = ReaderT rToMa
+    where
+      rToMa r = liftIO ioA
+
+instance MonadIO m => MonadIO (StateT s m) where
+  liftIO ioA = StateT smas
+    where
+      smas s = fmap (\a -> (a, s)) ma
+      ma = liftIO ioA
